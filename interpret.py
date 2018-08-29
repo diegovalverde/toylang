a979b13fe85bb0335c54094061a77c7b740780b1
dovg3 <diegovalverde@gatech.com>
2018-08-29 20:19:56 +0200

    new: Initial files @wip

---
+++ interpret.py
@@ -0,0 +1,35 @@
+from lark import Lark
+
+toylang_grammar = ''
+
+with open('toylang.lark', 'r') as myfile:
+    toylang_grammar = myfile.read()
+
+
+
+
+print('starting')
+
+grammar = Lark(toylang_grammar)
+text ="""
+# Fibo
+
+fibo(0) : 0 .
+ 
+fibo(1) : 1 . 
+
+fibo(N | N > 1): 
+    fibo( 1 ) .
+
+"""
+
+
+def run_instruction(t):
+    if t.data == 'function_definition':
+        print('funtion definition')
+
+
+parse_tree = grammar.parse(text)
+
+for inst in parse_tree.children:
+        run_instruction(inst)
