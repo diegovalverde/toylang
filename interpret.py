e8d0b9f3c1a48874f58b34b3cfb3448b304bdfe9
dovg3 <diegovalverde@gatech.com>
2018-08-29 20:28:32 +0200

    chg: playing with grammar @wip

--- interpret.py
+++ interpret.py
@@ -19,14 +19,14 @@ fibo(0) : 0 .
 fibo(1) : 1 . 
 
 fibo(N | N > 1): 
-    fibo( 1 ) .
+    fibo( N + 1 ) + fibo( N + 2 ).
 
 """
 
 
 def run_instruction(t):
     if t.data == 'function_definition':
-        print('funtion definition')
+        print('funtion definition..')
 
 
 parse_tree = grammar.parse(text)
